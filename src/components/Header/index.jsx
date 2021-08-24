122c84addfbe9541efffb971a1c0e6f6988af0de
Shamaa5 <djeims995@gmail.com>
2021-08-25 01:14:39 +0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <div>
+      <ul>
+        <li>home</li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
